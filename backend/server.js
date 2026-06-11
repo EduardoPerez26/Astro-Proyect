83e56446022ee9a96ce0653a33931673910fb618
EduardoPerez26 <eduardo.sis26@outlook.com>
2026-06-11 08:06:11 -0700

    ser

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,8 @@ app.use(cors({
     origin: [
         'http://localhost:4321',
         'http://localhost:3001',
-        'https://astro-proyect-akfs.vercel.app'
+        'https://astro-proyect-akfs.vercel.app',
+        'https://astro-proyect-tau.vercel.app'
     ],
     credentials: true,
     methods: ['GET','POST','PUT','DELETE','OPTIONS'],
