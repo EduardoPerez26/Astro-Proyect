20b628a4e24ea5e6eb5e935abefd5395493a28b0
EduardoPerez26 <eduardo.sis26@outlook.com>
2026-06-13 10:30:07 -0700

    as

--- public/js/conciliacion-popeyes.js
+++ public/js/conciliacion-popeyes.js
@@ -904,10 +904,18 @@ function generarConciliationPopeyes(salesData) {
             totalRevenue - paymentsTotal;
 
         const difference =
-        (row.cashExpected || 0 ) -
-        (row.cashDeposit || 0 ) -
-        (row.cashOverShortDebit || 0) +
-        (row.cashOverShortCredit || 0);
+            Math.round(
+                (
+                    row.cashExpected || 0
+                )
+                -
+                (
+                    (row.cashDeposit || 0) +
+                    (row.cashOverShortCredit || 0) -
+                    (row.cashOverShortDebit || 0)
+                )
+                * 100
+            ) / 100;
 
         return {
 
@@ -930,8 +938,7 @@ function generarConciliationPopeyes(salesData) {
 
             difference,
 
-            oS,
-            cashDepositCalculated
+            oS
 
         };
 
