3b7fb5503bab663de6bc9806a4e3ca05f8195b48
EduardoPerez26 <eduardo.sis26@outlook.com>
2026-07-15 11:48:54 -0700

    solucion de errores

--- backend/server.js
+++ backend/server.js
@@ -185,7 +185,7 @@ app.use((err, req, res, next) => {
 // INICIAR SERVIDOR
 // ============================================
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log('============================================');
     console.log(`Server running at http://localhost:${PORT}`);
